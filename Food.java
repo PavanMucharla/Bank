462b89ccefb97e8275311b031fc47e60f7810b27
Pavan <mucharlapavan6@gmail.com>
2024-02-11 12:43:27 +0530

    hi

---
+++ Food.java
@@ -0,0 +1,9 @@
+public class Food {
+    String name;
+    int cost;
+    public void item(){
+        if (name.equals("Briyani") && cost==200) {
+            System.out.println("Briyani");
+        }
+    }
+}
